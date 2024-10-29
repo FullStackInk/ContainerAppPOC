c018d5eb3f30f5ed62d7dd6833652f56db137eb9
Niranjith Perumal <nperumal@lodgelink.com>
2024-10-29 16:44:23 -0600

    app configuration

--- dist/schema.js
+++ dist/schema.js
@@ -2,6 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.schema = void 0;
 const graphql_yoga_1 = require("graphql-yoga");
+const dapr_1 = require("@dapr/dapr");
+// JS SDK does not support Configuration API over HTTP protocol yet
+const communicationProtocol = dapr_1.CommunicationProtocolEnum.GRPC;
+// Initialize Dapr client
+const daprHost = process.env.DAPR_HOST || "http://localhost";
+const daprPort = process.env.DAPR_HTTP_PORT || "3501";
+const DAPR_CONFIGURATION_STORE = "appconfigpoc";
+const CONFIGURATION_ITEMS = ["name"];
+// Function to get a configuration value from Azure App Configuration
+async function getConfigValue() {
+    const client = new dapr_1.DaprClient({ daprHost, daprPort, communicationProtocol });
+    // Get config items from the config store
+    try {
+        const config = await client.configuration.get(DAPR_CONFIGURATION_STORE, CONFIGURATION_ITEMS);
+        Object.keys(config.items).forEach((key) => {
+            console.log("Configuration for " + key + ":", JSON.stringify(config.items[key]));
+        });
+    }
+    catch (error) {
+        console.log("Could not get config item, err:" + error);
+    }
+}
+getConfigValue();
+// Example usage
+// Replace 'myKey' with the actual key in Azure App Configuration
 exports.schema = (0, graphql_yoga_1.createSchema)({
     typeDefs: /* GraphQL */ `
     type Query {
@@ -10,7 +35,7 @@ exports.schema = (0, graphql_yoga_1.createSchema)({
   `,
     resolvers: {
         Query: {
-            hello: () => 'world'
-        }
-    }
+            hello: () => "World",
+        },
+    },
 });
--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,42 @@
-import { createSchema } from 'graphql-yoga'
- 
+import { createSchema } from "graphql-yoga";
+
+import { CommunicationProtocolEnum, DaprClient } from "@dapr/dapr";
+
+// JS SDK does not support Configuration API over HTTP protocol yet
+const communicationProtocol = CommunicationProtocolEnum.GRPC;
+
+// Initialize Dapr client
+const daprHost = process.env.DAPR_HOST || "http://localhost";
+const daprPort = process.env.DAPR_HTTP_PORT || "3501";
+
+const DAPR_CONFIGURATION_STORE = "appconfigpoc";
+const CONFIGURATION_ITEMS = ["name"];
+
+// Function to get a configuration value from Azure App Configuration
+async function getConfigValue() {
+  const client = new DaprClient({ daprHost, daprPort, communicationProtocol });
+  // Get config items from the config store
+  try {
+    const config = await client.configuration.get(
+      DAPR_CONFIGURATION_STORE,
+      CONFIGURATION_ITEMS
+    );
+    Object.keys(config.items).forEach((key) => {
+      console.log(
+        "Configuration for " + key + ":",
+        JSON.stringify(config.items[key])
+      );
+    });
+  } catch (error) {
+    console.log("Could not get config item, err:" + error);
+  }
+}
+
+getConfigValue()
+
+// Example usage
+// Replace 'myKey' with the actual key in Azure App Configuration
+
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
     type Query {
@@ -8,7 +45,7 @@ export const schema = createSchema({
   `,
   resolvers: {
     Query: {
-      hello: () => 'world'
-    }
-  }
-})
+      hello: () => "World",
+    },
+  },
+});
